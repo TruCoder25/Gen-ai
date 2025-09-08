e2360969a3727493568c77fd080cb8b851605252
TruCoder25 <pratibhavishlavath25@gmail.com>
2025-09-08 16:59:23 +0530

    Completed the frontend,, going to work on backend to get the data

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,46 @@
-console.log('Working');
+const input = document.querySelector('#input');
+const chatContainer = document.querySelector('#chat-container')
+
+const ask = document.querySelector('#ask',handleAsk)
+
+
+
+input?.addEventListener('keyup',handleEnter)
+ask?.addEventListener('click', handleAsk);
+
+
+
+function generate(text){
+
+    const msg = document.createElement('div')
+    msg.className = `my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit`
+    msg.textContent = text
+
+    chatContainer?.appendChild(msg)
+    input.value=''
+    
+}
+
+function handleAsk(e)
+{
+    const text = input?.value.trim();
+
+    if(!text)
+        return;
+
+    generate(text);
+}
+
+function handleEnter(e)
+{
+    if(e.key === 'Enter')
+    {
+        const text = input?.value.trim();
+
+        if(!text)
+            return;
+
+
+         generate(text);
+    }
+}
